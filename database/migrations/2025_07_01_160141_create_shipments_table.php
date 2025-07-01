3c21dc6ac5940c1e315d19a59e6f553ad1ab0cfd
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-01 13:03:25 -0300

    #16 -Creacion de la migracion shipments.

---
+++ database/migrations/2025_07_01_160141_create_shipments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\ShipmentStatusEmun;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('tracking_number')->nullable();
+            $table->enum('status', ShipmentStatusEmun::allValuesAsArray())->default(ShipmentStatusEmun::default());
+            $table->timestamp('shipped_at')->nullable();
+            $table->timestamp('delivered_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipments');
+    }
+};
