1be7f5a721ebd888c5b3a9cf6fde1d4254858bbf
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-01 12:17:23 -0300

    #11 -Creacion de la migracion addresses.

---
+++ database/migrations/2025_07_01_151641_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('recipient_name');
+            $table->string('line1');
+            $table->string('line2');
+            $table->string('city');
+            $table->string('province');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
