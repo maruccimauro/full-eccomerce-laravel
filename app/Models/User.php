d4bad218d7c4874be012fc6d247f4e8de41d281c
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-02 12:46:14 -0300

    #43 feat(model):Modificación de Relaciones en Modelo Existente User

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,20 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
+    }
 }
