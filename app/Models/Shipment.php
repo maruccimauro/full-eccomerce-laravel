b876208fa462d8f6660054413a019f606ebbccb4
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-02 12:40:17 -0300

    #41 feat(model): Creación de Modelo y Relaciones Shipment

---
+++ app/Models/Shipment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shipment extends Model
+{
+    protected $fillable = [
+        'order_id',
+        'tracking_number',
+        'status',
+        'shipped_at',
+        'delivered_at'
+    ];
+
+    public function Order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
