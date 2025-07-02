89c784d6032e499b47efecdde5bd34bc0e9c81c6
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-02 12:31:43 -0300

    #38 feat(model):Creación de Modelo y Relaciones Payment

---
+++ app/Models/Payment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $fillable = [
+        'order_id',
+        'payment_gateway',
+        'transaction_id',
+        'amount',
+        'status',
+        'paid_at'
+    ];
+
+
+    public function Order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
