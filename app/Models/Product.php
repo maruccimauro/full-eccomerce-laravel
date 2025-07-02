321e4fb3d70e2712263aef65b03a3995b64584f4
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-02 12:35:25 -0300

    #39 feat(model): Creación de Modelo y Relaciones Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,4 +22,24 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function productImages()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
 }
