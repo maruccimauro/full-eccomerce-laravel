e777015fc52bf0be4e2d939d457d4ed5dfdb1f7b
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-02 00:10:41 -0300

    #32 feat: se agrego la relacion products al modelo Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -22,4 +22,9 @@ class Category extends Model
     {
         return $this->belongsTo(Category::class, 'parent_id', 'id');
     }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
