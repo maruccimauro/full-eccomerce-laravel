132ec8840df22a04a2d076b815009184948e503b
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 20:44:36 -0300

    #61 feat(enum): Crear CategoryEnum para campos del modelo Category

---
+++ app/Enums/CategoryEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+class CategoryEnum
+{
+  public const ID = 'id';
+  public const NAME = 'name';
+  public const SLUG = 'slug';
+  public const PARENT_ID = 'parent_id';
+  public const CREATED_AT = 'created_at';
+  public const UPDATED_AT = 'updated_at';
+}
