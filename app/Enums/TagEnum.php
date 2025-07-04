cb685a380eb4a7faa715f8bfec1d4e7d69d780b2
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 23:45:19 -0300

    #91 feat(enum): Crear TagEnum para campos del módulo Tag

---
+++ app/Enums/TagEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+class TagEnum
+{
+  public const ID = 'id';
+  public const NAME = 'name';
+  public const CREATED_AT = 'created_at';
+  public const UPDATED_AT = 'updated_at';
+}
