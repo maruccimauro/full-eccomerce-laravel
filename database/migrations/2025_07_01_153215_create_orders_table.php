c4ecf1aaf38e86603f2a9c25543a525ac83d0a85
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-04 11:05:19 -0300

    #103 refactor(migration): Actualizar columna payment_status a OrderPaymentStatusEnum en la tabla orders.

--- database/migrations/2025_07_01_153215_create_orders_table.php
+++ database/migrations/2025_07_01_153215_create_orders_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Enums\OrderStatusEnum;
+use App\Enums\OrderPaymentStatusEnum;
 
 return new class extends Migration
 {
@@ -19,7 +20,7 @@ return new class extends Migration
             $table->decimal('total', 10, 2);
             $table->enum('status', OrderStatusEnum::allValuesAsArray())->default(OrderStatusEnum::default());
             $table->string('payment_method');
-            $table->string('payment_status');
+            $table->enum('payment_status', OrderPaymentStatusEnum::allValuesAsArray())->default(OrderPaymentStatusEnum::default());
             $table->timestamps();
         });
     }
