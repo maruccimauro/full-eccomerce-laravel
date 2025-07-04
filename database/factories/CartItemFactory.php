5aa99a1a4b61d2188a89b7d055ce2a971493216a
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 21:41:50 -0300

    #74 feat(factory-model): Crear CartItemFactory y aplicar HasFactory en el model CartItem

---
+++ database/factories/CartItemFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Enums\CartItemEnum;
+use App\Models\Cart;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CartItem>
+ */
+
+class CartItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            CartItemEnum::QUANTITY => fake()->numberBetween(1, 20)
+        ];
+    }
+}
