ba0afacbac89d2b0a3fd1f1d5ee6f50daa6c76e2
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 21:08:12 -0300

    #64 feat(enum): Crear ProductEnum para campos del módulo Product

---
+++ app/Enums/ProductEnum.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+
+class ProductEnum
+{
+  public const ID = 'id';
+  public const NAME = 'name';
+  public const SLUG = 'slug';
+  public const DESCRIPTION = 'description';
+  public const PRICE = 'price';
+  public const STOCK = 'stock';
+  public const CATEGORY_ID = 'category_id';
+  public const IS_ACTIVE = 'is_active';
+  public const CREATED_AT = 'created_at';
+  public const UPDATED_AT = 'updated_at';
+}
