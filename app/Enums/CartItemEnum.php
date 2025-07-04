2467148ed9d295aa141b107b8241992871f66c59
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 21:37:54 -0300

    #73 feat(factory-model): Crear CartItemEnum para campos del módulo CartItem

---
+++ app/Enums/CartItemEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+class CartItemEnum
+{
+  public const ID = 'id';
+  public const CART_ID = 'cart_id';
+  public const PRODUCT_ID = 'product_id';
+  public const QUANTITY = 'quantity';
+  public const CREATED_AT = 'created_at';
+  public const UPDATED_AT = 'updated_at';
+}
