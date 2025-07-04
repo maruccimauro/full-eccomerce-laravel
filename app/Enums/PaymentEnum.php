f901f797f2118841b579fccb7c22bb473cca410d
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 23:39:06 -0300

    #88 feat(enum):Crear PaymentEnum para campos del módulo Payment

---
+++ app/Enums/PaymentEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+class PaymentEnum
+{
+  public const ID = 'id';
+  public const ORDER_ID = 'order_id';
+  public const PAYMENT_GATEWAY = 'payment_gateway';
+  public const TRANSACTION_ID = 'transaction_id';
+  public const AMOUNT = 'amount';
+  public const STATUS = 'status';
+  public const PAID_AT = 'paid_at';
+  public const CREATED_AT = 'created_at';
+  public const UPDATED_AT = 'updated_at';
+}
