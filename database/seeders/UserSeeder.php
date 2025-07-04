168a1c10a39939506709a0135cccacf27963cd2c
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-04 18:07:11 -0300

    #117 refactor(seed): Actualizacion de UserSeeder para Roles uso global e inmediato en Modulo Category.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,12 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserRoleEnum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Fields\UserFields;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -17,7 +19,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
 
-        User::create([
+        $admin = User::create([
             UserFields::NAME => 'admin',
             UserFields::EMAIL => 'admin@gmail.com',
             UserFields::EMAIL_VERIFIED_AT => now(),
@@ -25,7 +27,8 @@ class UserSeeder extends Seeder
             UserFields::REMEMBER_TOKEN => Str::random(10),
             UserFields::PHONE => fake()->phoneNumber(),
         ]);
-
+        Role::create(['name' => UserRoleEnum::ADMIN]);
+        $admin->assignRole(UserRoleEnum::ADMIN);
         User::create([
             UserFields::NAME => 'customer',
             UserFields::EMAIL => 'customer@gmail.com',
