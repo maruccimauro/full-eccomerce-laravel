1fbdb862525dbdbd3d2c9f08d3a4eca41effecd1
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 21:59:26 -0300

    #82 feat(enum): Crear OrderItemEnum para campos del módulo OrderItem

---
+++ app/Enums/OrderItemEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+class OrderItemEnum
+{
+  public const ID = 'id';
+  public const ORDER_ID = 'order_id';
+  public const PRODUCT_ID = 'product_id';
+  public const QUANTITY = 'quantity';
+  public const UNIT_PRICE = 'unit_price';
+  public const TOTAL_PRICE = 'total_price';
+  public const CREATED_AT = 'created_at';
+  public const UPDATED_AT = 'updated_at';
+}
