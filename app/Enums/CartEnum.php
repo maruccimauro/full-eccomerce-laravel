eb477f9e8d074ec444ac5471985e429106f10428
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 21:31:52 -0300

    #70 feat(enum): Crear CartEnum para campos del módulo Cart

---
+++ app/Enums/CartEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+class CartEnum
+{
+  public const ID = 'id';
+  public const USER_ID = 'user_id';
+  public const CREATED_AT = 'created_at';
+  public const UPDATED_AT = 'updated_at';
+}
