aeec311c2a78c663d424633cdaf877c2cc2f298c
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-03 22:16:09 -0300

    #87 feat(seed): Crear ShipmentSeed para poblar el módulo de shipment

---
+++ database/seeders/ShipmentSeed.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\OrderEnum;
+use App\Enums\ShipmentEnum;
+use App\Models\Order;
+use App\Models\Shipment;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ShipmentSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $orders = Order::all();
+        if ($orders->count() === 0) {
+            throw new \Exception('Se necesitan orders para ejecuar ' . static::class);
+        }
+        $orders->each(function ($order) {
+            Shipment::factory()->create([
+                ShipmentEnum::ORDER_ID => $order->{OrderEnum::ID}
+            ]);
+        });
+    }
+}
