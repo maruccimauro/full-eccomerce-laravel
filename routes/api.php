c09d43d01fb6292a1969bb90e522112a96d82e3f
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-05 20:45:27 -0300

    #147 feat(route): Creacion de Rutas API para el modulo ProductImage

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductImageController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -23,4 +24,15 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/products/{product}', [ProductController::class, 'show']);
     Route::put('/products/{product}', [ProductController::class, 'update']);
     Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+});
+
+
+//ProductImage
+Route::get('/productImages', [ProductImageController::class, 'index']);
+Route::get('/productImages/{productImage}', [ProductImageController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/productImages', [ProductImageController::class, 'store']);
+    Route::put('/productImages/{productImage}', [ProductImageController::class, 'update']);
+    Route::delete('/productImages/{productImage}', [ProductImageController::class, 'destroy']);
 });
