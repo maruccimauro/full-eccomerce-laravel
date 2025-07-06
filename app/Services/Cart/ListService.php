8f6c408e7ed8beb9b9419fd9a97bde97962930ca
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-05 22:50:57 -0300

    #154 feat(service): Creacion de ListService para el modulo Cart

---
+++ app/Services/Cart/ListService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services\Cart;
+
+use App\Models\Cart;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+class ListService
+{
+  public function execute()
+  {
+    if (!Gate::allows('viewany', Cart::class)) {
+      throw new HttpResponseException(response()->json(['message' => 'No tienes autorizacion para realizar esta accion.'], Response::HTTP_FORBIDDEN));
+    }
+
+    return Cart::all();
+  }
+}
