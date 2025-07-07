76a9904521a1076b404529a23459183acf3042e9
MARUCCI MAURO NAHUEL <marucci.mauro@gmail.com>
2025-07-06 21:58:40 -0300

    #163 feat(route): Reorganizar rutas del archivo api.php por recurso y tipo de acceso

--- routes/api.php
+++ routes/api.php
@@ -9,24 +9,32 @@ use App\Http\Controllers\ProductImageController;
 use App\Http\Controllers\CartController;
 
 
+//Auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
+//addresses
 Route::get('/addresses', [AddressController::class, 'index']);
-Route::get('/products', [ProductController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
-
     Route::post('/addresses', [AddressController::class, 'store']);
     Route::get('/addresses/{address}', [AddressController::class, 'show']);
     Route::put('/addresses/{address}', [AddressController::class, 'update']);
     Route::delete('/addresses/{address}', [AddressController::class, 'destroy']);
+});
 
+
+//products
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/products', [ProductController::class, 'store']);
-    Route::get('/products/{product}', [ProductController::class, 'show']);
     Route::put('/products/{product}', [ProductController::class, 'update']);
     Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 });
+
 
 
 //ProductImage
@@ -40,7 +48,7 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 
-//cart
+//ProductImage
 Route::get('/carts', [cartController::class, 'index']);
 Route::get('/carts/{cart}', [cartController::class, 'show']);
 
